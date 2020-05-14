d64435860d927800a348e21dd59b28d8ab385de2
Jarrod Noonan <jarrod@noonan.co>
2020-05-15 00:46:00 +0100

    bump

--- location-domination.php
+++ location-domination.php
@@ -4,7 +4,7 @@
 Plugin Name: Location Domination
 Plugin URI: #
 Description: An iAutoM8 plugin designed to make mass page generating easy!
-Version: 1.56
+Version: 1.57
 Author: iAutoM8
 Author URI: https://i-autom8.com
 License: GPL2
@@ -14,7 +14,7 @@ if ( ! defined('WPINC') ){
 	die;
 }
 
-define( 'LOCATION_DOMINATION_VER', 1.56 );
+define( 'LOCATION_DOMINATION_VER', 1.57 );
 
 /*
  * Include the core classes
