6f6e20131138a3a9786884df0ecce9eb6fe35d96
Jarrod Noonan <jarrod@noonan.co>
2020-06-17 17:54:47 +0100

    Incremental

--- metaboxes/views/settings.php
+++ metaboxes/views/settings.php
@@ -1,7 +1,7 @@
 <?php
     global $post, $wpdb;
 
-    $index_table_name = $wpdb->prefix . LOCATION_DOMINATION_INDEX_DB_TABLE;
+//    $index_table_name = $wpdb->prefix . LOCATION_DOMINATION_INDEX_DB_TABLE;
 
     $previous_request_meta = get_post_meta( $post->ID, 'location_domination_post_request', true );
     $previous_request =  $previous_request_meta ? json_encode( $previous_request_meta ) : 'null';
@@ -10,9 +10,9 @@
 
     $post_type = get_post_meta( $post->ID, '_uuid', true );
     $post_type_post_count = wp_count_posts( $post_type );
-    $index_matches = $wpdb->get_var( "SELECT COUNT(ID) FROM $index_table_name WHERE post_type = '$post_type'" );
-    $requires_indexing = (int) $post_type_post_count->publish !== $index_matches;
+//    $index_matches = $wpdb->get_var( "SELECT COUNT(ID) FROM $index_table_name WHERE post_type = '$post_type'" );
+//    $requires_indexing = (int) $post_type_post_count->publish !== $index_matches;
 ?>
 <div class="location-domination-vendor" id="location-domination-settings">
-    <post-builder :requires-indexing="<?php echo $requires_indexing ? 'false' : 'true'; ?>" :previous-request-transient="<?php echo htmlentities($previous_request_transient); ?>" :previous-request="<?php echo htmlentities($previous_request); ?>" nonce="<?php echo wp_create_nonce( 'location-domination-start-queue' ); ?>" ajax-url="<?php echo admin_url('admin-post.php'); ?>" template-id="<?php echo $post->ID ?>" />
+    <post-builder :requires-indexing="<?php echo true ? 'false' : 'true'; ?>" :previous-request-transient="<?php echo htmlentities($previous_request_transient); ?>" :previous-request="<?php echo htmlentities($previous_request); ?>" nonce="<?php echo wp_create_nonce( 'location-domination-start-queue' ); ?>" ajax-url="<?php echo admin_url('admin-post.php'); ?>" template-id="<?php echo $post->ID ?>" />
 </div>
