8ddf3768dde979c1373d2ee48079f238aeca4651
Jarrod Noonan <jarrod@noonan.co>
2020-11-16 22:23:37 +0000

    Bug fixes

--- includes/class-location-domination-spinner.php
+++ includes/class-location-domination-spinner.php
@@ -14,8 +14,7 @@ class Location_Domination_Spinner {
     /**
      * The regex pattern used for grabbing spintaxs
      */
-    /*        const REGEX_PATTERN = '/\{(((?>[^\{\}]+)|(?R))*?)\}/xu';*/
-    const REGEX_PATTERN = '/(__|_\[)?\{(((?>[^\{\}]+)|(?R))*?)\}(__|\]_)?/xu';
+    const REGEX_PATTERN = '/\{(((?>[^\{\}]+)|(?R))*?)\}/xu';
 
     /**
      * @param $content
@@ -30,7 +29,7 @@ class Location_Domination_Spinner {
             mt_srand( $integer_seed );
         }
 
-        return preg_replace_callback( self::REGEX_PATTERN, [
+        return preg_replace_callback( self::get_regex_pattern(), [
             Location_Domination_Spinner::class,
             'replace',
         ], $content );
@@ -53,6 +52,14 @@ class Location_Domination_Spinner {
         $parts = explode( '|', $text );
 
         return $parts[ array_rand( $parts ) ];
+    }
+
+    static function get_regex_pattern() {
+        if ( class_exists( 'FLBuilderModel' ) && FLBuilderModel::is_builder_enabled() ) {
+            return '/(__|_\[)?\{(((?>[^\{\}]+)|(?R))*?)\}(__|\]_)?/xu';
+        }
+
+        return '/\{(((?>[^\{\}]+)|(?R))*?)\}/xu';
     }
 
 }
