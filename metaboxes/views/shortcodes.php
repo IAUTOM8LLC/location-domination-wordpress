9edd901cf0bb93b81fd25c44af073b9bc0f2236c
Manish <manishgautam.work@gmail.com>
2023-09-12 16:57:18 +0530

    Show citymeta shortcode list

--- metaboxes/views/shortcodes.php
+++ metaboxes/views/shortcodes.php
@@ -10,6 +10,10 @@
         <li><code>[ld_map]</code>Use to get a Google Maps embed of current location</li>
         <li><code>[zips]</code>Use to get a list of all ZIP codes related to the city</li>
         <li><code>[breadcrumb]</code>Use to display a breadcrumb by city, state, county for current page.</li>
-        <li><code>[neighborhoods]</code> Use to display a list of neighborhoods (US only)</li>
+        <li><code>[neighborhoods]</code> Use to display a list of neighborhoods - US ONLY</li>
+        <li><code>[citymeta name="attribute"]</code> Use to display city related details by attribute name - US ONLY</li>
+        <li>Example:<code>[citymeta name="population"]</code>, will display population of the city.</li>
+        <li><strong>List of supported attributes</strong></li>
+        <li>lat, lng, population, density, source, military, incorporated, cdp, township, timezone, ranking, age_median, age_under_10, age_10_to_19, age_20s, age_30s, age_40s, age_50s, age_60s, age_70s, age_over_80, male, female, married, divorced, never_married, widowed, family_size, family_dual_income, income_household_median, income_household_under_5, income_household_5_to_10, income_household_10_to_15, income_household_15_to_20, income_household_20_to_25, income_household_25_to_35, income_household_35_to_50, income_household_50_to_75, income_household_75_to_100, income_household_100_to_150, income_household_150_over, income_household_six_figure, income_individual_median, home_ownership, home_value, rent_median, education_less_highschool, education_highschool, education_some_college, education_bachelors, education_graduate, education_college_or_above, education_stem_degree, labor_force_participation, unemployment_rate, race_white, race_black, race_asian, race_native, race_pacific, race_other, race_multiple, hispanic, disabled, poverty, limited_english, commute_time, health_uninsured, veteran, cbsa_fips, cbsa_name, cbsa_metro, csa_fips, csa_name</li>
     </ul>
 </div>
